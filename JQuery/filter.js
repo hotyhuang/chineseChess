83addfb38b831701be0480a65304d0750706b518
hotyhuang <hotyhuang@gmail.com>
2017-03-04 13:48:08 -0500

    upload chess project to repo

---
+++ JQuery/filter.js
@@ -0,0 +1,17 @@
+$(function(){
+	$("#filter li").click(function(){
+		var category = $(this).html();
+		
+		$("#filter li").removeClass("active");
+		$(this).addClass("active");
+		
+		$("#portfolio li").hide();
+		
+		/*$("#portfolio li").each(function(){
+			if($(this).hasClass(category)){
+				$(this).fadeIn();
+			}
+		});*/
+		$('#portfolio').find('.' + category).fadeIn();
+	});
+});
---
+++ JQuery/script.js
@@ -0,0 +1,67 @@
+var username="";
+
+	function send_message(message){
+		var prevState=$("#container").html();
+		
+		if(prevState.length !=0){
+			prevState+="<br>";
+		}
+		
+		$("#container").html(prevState +"<span class='current_message'>"+"<span class='bot'>Chatbot: </span>"+ message+"</span>");
+		$(".current_message").hide();
+		$(".current_message").delay(500).fadeIn();
+		$(".current_message").removeClass("current_message");
+	}
+
+	function get_username(){
+		send_message("Hello, your name?");
+	}
+	
+	function ai(message){
+		if(username.length==0){
+			username=message;
+			send_message("Nice to meet you "+ username +", how are you?");
+		}
+		
+		if(message.indexOf("how are you")>=0){
+			send_message("Thank, I am good.");
+		}
+		
+		if((message.indexOf("time")>=0) || (message.indexOf("hours")>=0)){
+			var date=new Date();
+			var h=date.getHours();
+			var m=date.getMinutes();
+			send_message("Current time is: "+h+":"+m+",");
+		}
+	}
+
+	$(function(){
+		get_username();
+		
+		$("#textbox").keypress(function(event){
+			if(event.which==13){
+				if($("#enter").prop("checked")){
+					$("#send").click();
+					//console.log("enter pressed");
+					event.preventDefault();
+				}
+			}
+		});
+		
+		$("#send").click(function(){
+			var username="<span class='username'>You: </span>";
+			var newMessage=$("#textbox").val();
+			var prevState=$("#container").html();
+			
+			if(prevState.length !=0){
+				prevState+="<br>";
+			}
+			
+			$("#container").html(prevState +username+ newMessage);
+			$("#container").scrollTop($("#container").prop("scrollHeight"));
+			
+			$("#textbox").val("");
+			
+			ai(newMessage);
+		});
+	});
